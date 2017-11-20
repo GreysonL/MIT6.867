449efc4b86917957035b61152c64724fdca4eb69
GreysonL <gang_liu@g.harvard.edu>
2017-11-19 19:58:00 -0500

    binary

--- Code/3. convert categorical variable to one-hot vector.R
+++ Code/3. convert categorical variable to one-hot vector.R
@@ -1,30 +1,36 @@
-setwd("~/GitHub/MIT6.867/Data")
-
 ## toy example 
 a=c(1,2,3,1,2,3,4,1,2,3)
 b=c(1,2,3,4,1,2,3,4,1,2)
-d=c(1,2,3,1,2,3,3,1,2,1)
+d=c(1,2,2,1,2,1,1,1,2,1)
 id=c(1,2,3,4,5,6,7,8,9,10)
 f=cbind(id,a,b,d)
+cat_varname=c('a','b','d')
 colnames(f)=c("id","a","b","d")
 f=data.frame(f)
 for (i in 1:length(cat_varname)){
   one_hot=c()
   drop = cat_varname[i]
   cat_var=f[,drop]
-  f=f[,!(names(f) %in% drop)]
   cat_range=unique(cat_var)
-  for (j in 1:length(cat_range)){
-    one_hot=cbind(one_hot,as.numeric(cat_var==cat_range[j]))
+  if (length(cat_range)==2){
+    f[cat_var==cat_range[1],drop]=1
+    f[cat_var==cat_range[2],drop]=-1
   }
-  one_hot=data.frame(one_hot)
-  for (j in 1:length(cat_range)){
-    colnames(one_hot)[j]=paste0(drop,"_",cat_range[j])
+  else{
+    f=f[,!(names(f) %in% drop)]
+    for (j in 1:length(cat_range)){
+      one_hot=cbind(one_hot,as.numeric(cat_var==cat_range[j]))
+    }
+    one_hot=data.frame(one_hot)
+    for (j in 1:length(cat_range)){
+      colnames(one_hot)[j]=paste0(drop,"_",cat_range[j])
+    }
+    f=cbind(f,one_hot)
   }
-  f=cbind(f,one_hot)
 }
 
 ## real data - trainset 
+setwd("~/GitHub/MIT6.867/Data")
 data = read.csv("trainset_impute50000.csv",header=T)
 cat_varname=c('ps_ind_02_cat','ps_ind_04_cat','ps_ind_05_cat','ps_ind_06_bin','ps_ind_07_bin',
               'ps_ind_08_bin','ps_ind_09_bin','ps_ind_10_bin','ps_ind_11_bin','ps_ind_12_bin',
@@ -37,16 +43,22 @@ for (i in 1:length(cat_varname)){
   one_hot=c()
   drop = cat_varname[i]
   cat_var=data[,drop]
-  data=data[,!(names(data) %in% drop)]
   cat_range=unique(cat_var)
-  for (j in 1:length(cat_range)){
-    one_hot=cbind(one_hot,as.numeric(cat_var==cat_range[j]))
+  if (length(cat_range)==2){
+    data[cat_var==cat_range[1],drop]=1
+    data[cat_var==cat_range[2],drop]=-1
   }
-  one_hot=data.frame(one_hot)
-  for (j in 1:length(cat_range)){
-    colnames(one_hot)[j]=paste0(drop,"_",cat_range[j])
+  else{
+    data=data[,!(names(data) %in% drop)]
+    for (j in 1:length(cat_range)){
+      one_hot=cbind(one_hot,as.numeric(cat_var==cat_range[j]))
+    }
+    one_hot=data.frame(one_hot)
+    for (j in 1:length(cat_range)){
+      colnames(one_hot)[j]=paste0(drop,"_",cat_range[j])
+    }
+    data=cbind(data,one_hot)
   }
-  data=cbind(data,one_hot)
 }
 write.csv(data,"trainset_impute_onehot.csv",row.names = F)
 
@@ -63,16 +75,22 @@ for (i in 1:length(cat_varname)){
   one_hot=c()
   drop = cat_varname[i]
   cat_var=data[,drop]
-  data=data[,!(names(data) %in% drop)]
   cat_range=unique(cat_var)
-  for (j in 1:length(cat_range)){
-    one_hot=cbind(one_hot,as.numeric(cat_var==cat_range[j]))
+  if (length(cat_range)==2){
+    data[cat_var==cat_range[1],drop]=1
+    data[cat_var==cat_range[2],drop]=-1
   }
-  one_hot=data.frame(one_hot)
-  for (j in 1:length(cat_range)){
-    colnames(one_hot)[j]=paste0(drop,"_",cat_range[j])
+  else{
+    data=data[,!(names(data) %in% drop)]
+    for (j in 1:length(cat_range)){
+      one_hot=cbind(one_hot,as.numeric(cat_var==cat_range[j]))
+    }
+    one_hot=data.frame(one_hot)
+    for (j in 1:length(cat_range)){
+      colnames(one_hot)[j]=paste0(drop,"_",cat_range[j])
+    }
+    data=cbind(data,one_hot)
   }
-  data=cbind(data,one_hot)
 }
 colnames(data)[1]='id'
 write.csv(data,"testset_impute_onehot.csv",row.names = F)
