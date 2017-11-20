42dc510b775d207c8039221239f909fb6543674a
GreysonL <gang_liu@g.harvard.edu>
2017-11-19 22:42:13 -0500

    real data

--- Code/simulation_greyson.R
+++ Code/simulation_greyson.R
@@ -1,12 +1,20 @@
+set.seed(867)
 N=3000
 x1=rnorm(N,mean=0,sd=1)
 x2=rnorm(N,mean=0,sd=1)
 x3=rnorm(N,mean=0,sd=1)
 x4_raw=sample(c(1,2,3,4),N,replace=T)
 x4_hot=c()
+x5=rnorm(N,mean=0,sd=1)
+x6_hot=c()
+x6_raw=sample(c(1,2,3,4),N,replace=T)
 cat_range=unique(x4_raw)
 for (j in 1:length(cat_range)){
   x4_hot=cbind(x4_hot,as.numeric(x4_raw==cat_range[j]))
+}
+cat_range=unique(x6_raw)
+for (j in 1:length(cat_range)){
+  x6_hot=cbind(x6_hot,as.numeric(x6_raw==cat_range[j]))
 }
 
 ## assume there is an interaction between x1 and x2, x3 and x4, while the main effects of x1,x2,x3,x4 
@@ -18,9 +26,9 @@ for (i in 1:N){
   inter2=rbind(inter2,x3[i]*x4_hot[i,])
 }
 
-X=cbind(x1,x2,x3,x4_hot,inter1,inter2)
+X=cbind(x1,x2,x3,x4_hot,inter1,inter2,x5,x6_hot)
 beta=c(0.1,-0.1,0.1,-0.1,0.1,-0.1,0.1,2,0.5,1,1.5,2)
-eta=-0.5+X%*%beta+rnorm(N,mean=0,sd=1)
+eta=-0.5+X[,1:12]%*%beta+rnorm(N,mean=0,sd=1)
 p=exp(eta)/(1+exp(eta))
 mean(p)
 Y=as.numeric(p>0.5)
